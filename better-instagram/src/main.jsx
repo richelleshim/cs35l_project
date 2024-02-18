d90186cfc909ff26ac173c6ce9b21389caea77ba
danielfenex <68931226+danielfenex@users.noreply.github.com>
2024-02-18 13:45:42 -0800

    BrowserRouter implemented
    imported BrowserRouter from 'react-router-dom'
    
    wrapped <App /> with <BrowserRouter><BrowserRouter/>

--- better-instagram/src/main.jsx
+++ better-instagram/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 // add themeing capabilities similar to what Richelle did
 import { CssVarsProvider } from "@mui/joy"
@@ -10,8 +11,10 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <CssVarsProvider theme={theme}> {/* Add theme to app */}
       <App />
     </CssVarsProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
