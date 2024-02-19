66ab2fdc2776468da5c7eee1b515dec67bd8db7a
mohelizabeth <elizabethxmoh@gmail.com>
2024-02-18 16:58:18 -0800

    added posts feature

--- better-instagram/src/App.jsx
+++ better-instagram/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './components/react.svg'
 import viteLogo from '/vite.svg'
 
 import HomePageWidget from './components/HomePage/HomePageWidget'
-
+import ProfilePage from './components/pages/ProfilePage'
 import './App.css'
 
 function App() {
@@ -11,27 +11,7 @@ function App() {
 
   return (
     <>
-      <HomePageWidget></HomePageWidget>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Bruingram</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ProfilePage />
     </>
   )
 }
