dbf656c78aa29b5658467a2dd558ad9e49dbb510
Chai112 <sirdesignpie@gmail.com>
2024-02-18 19:44:12 -0800

    add personalities

--- better-instagram/src/App.jsx
+++ better-instagram/src/App.jsx
@@ -6,7 +6,7 @@ import viteLogo from '/vite.svg'
 import HomePageWidget from './components/HomePage/HomePageWidget'
 
 // import from MUI
-import { Stack } from "@mui/joy";
+import { Stack, Box } from "@mui/joy";
 
 import './App.css'
 
@@ -15,17 +15,27 @@ function App() {
 
   return (
     <>
-        <Stack direction="row" spacing={3}>
-            <Stack direction="column" spacing={3} alignItems="flex-start" justifyContent="center">
-              <HomePageWidget name="Greg Heffley" major="Computer Science" year="24"></HomePageWidget>
-              <HomePageWidget name="Jennifer Lee" major="Cognitive Science" year="26"></HomePageWidget>
-            </Stack>
-            <Stack direction="column" spacing={3} alignItems="flex-start" justifyContent="center">
-              <HomePageWidget name="Richelle Shim" major="Chicano Studies" year="26"></HomePageWidget>
-              <HomePageWidget name="Chaidhat" major="Colarado Studies" year="26"></HomePageWidget>
-            </Stack>
-        </Stack>
-      <h1>Bruingram</h1>
+      <Box sx={{ pb: 5 }}>
+        <img
+          src="public/Bruingram.png"
+          loading="lazy"
+          alt=""
+        />
+      </Box>
+      <Stack direction="column" spacing={3}>
+          <Stack direction="row" spacing={3} alignItems="flex-start" justifyContent="center">
+            <HomePageWidget name="Daniel Fenex" desc="Likes cats." major="Computer Science" year="24" imageSrc="daniel"></HomePageWidget>
+            <HomePageWidget name="Jennifer Lee" desc="I don't think YRL is good." major="Cognitive Science" year="26" imageSrc="jennifer"></HomePageWidget>
+          </Stack>
+          <Stack direction="row" spacing={3} alignItems="flex-start" justifyContent="center">
+            <HomePageWidget name="Richelle Shim" desc="Hates dogs." major="Chicano Studies" year="26" imageSrc="richelle"></HomePageWidget>
+            <HomePageWidget name="Michael J. Jordan" desc="Takes things personally." major="Chemical Sciences" year="26" imageSrc="mjj"></HomePageWidget>
+          </Stack>
+          <Stack direction="row" spacing={3} alignItems="flex-start" justifyContent="center">
+            <HomePageWidget name="Michael B. Jordan" desc='"Whats ur number bb"' major="Colorado Studies" year="26" imageSrc="mjb"></HomePageWidget>
+            <HomePageWidget name="Chai Chai" desc="I eat hamburgers." major="Cantonese Studies" year="26" imageSrc="chai"></HomePageWidget>
+          </Stack>
+      </Stack>
     </>
   )
 }
--- better-instagram/src/components/HomePage/HomePageWidget.jsx
+++ better-instagram/src/components/HomePage/HomePageWidget.jsx
@@ -2,53 +2,56 @@ import { useState } from 'react'
 
 // import all custom styling from MUI
 import { Card, Stack, Typography, AspectRatio, Grid, IconButton, Divider } from "@mui/joy";
-import { Favorite, FavoriteBorder, School, Backpack } from '@mui/icons-material';
+import { FavoriteRounded, FavoriteBorderRounded, SchoolOutlined, BackpackOutlined } from '@mui/icons-material';
 
 function LikeIcon ({ liked }) {
     if (liked) {
-        return <Favorite sx={{ fontSize: 30, color: "red" }} />;
+        return <FavoriteRounded sx={{ fontSize: 30, color: "red" }} />;
     } else {
-        return <FavoriteBorder sx={{ fontSize: 30 }} />;
+        return <FavoriteBorderRounded sx={{ fontSize: 30 }} />;
     }
 }
 
-function HomePageWidget ({ name, major, year }) {
+function HomePageWidget ({ name, desc, major, year, imageSrc }) {
     const [liked, setLiked] = useState(false);
+    const imageSrcFull = `assets/profilepics/${imageSrc}.png`
 
     return <>
     <Card 
         sx={{
             boxShadow: 'lg',
-            px: 3,
-            py: 5
+            px: 4,
+            py: 4
         }}
     >
         <Grid container spacing={2}>
             <Grid item>
-                <Stack direction="row" spacing={5} sx={{ width: 500, height: 200 }}>
+                <Stack direction="row" alignItems="center" spacing={5} sx={{ width: 500, height: 200 }}>
                     <AspectRatio
                         ratio="1"
-                        sx={{ flex: 1, maxWidth: 200, borderRadius: '100%' }}
+                        sx={{ flex: 1, maxWidth: 180, borderRadius: '100%' }}
                     >
                         <img
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                        srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
+                        src={imageSrcFull}
                         loading="lazy"
                         alt=""
                         />
                     </AspectRatio>
-                    <Stack direction="column" alignItems="flex-start" justifyContent="center">
-                        <Typography level="h2" textAlign="start" sx={{ pb: 2 }}>
+                    <Stack direction="column" alignItems="flex-start" justifyContent="center" spacing={1}>
+                        <Typography level="h2" textAlign="start">
                             {name}
                         </Typography>
-                        <Stack direction="row" spacing={2}>
-                            <Backpack sx={{ fontSize: 30 }} />
+                        <Typography level="p" textAlign="start" sx={{ pb: 1.5 }}>
+                            {desc}
+                        </Typography>
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <BackpackOutlined sx={{ fontSize: 30 }} />
                             <Typography level="p">
                                 {major}
                             </Typography>
                         </Stack>
-                        <Stack direction="row" spacing={2}>
-                            <School sx={{ fontSize: 30 }} />
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <SchoolOutlined sx={{ fontSize: 30 }} />
                             <Typography level="p">
                                 Class of '{year}
                             </Typography>
