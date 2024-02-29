f86f0d6cc7ae02e9d6a90f16b5ae63638d6e935e
danielfenex <68931226+danielfenex@users.noreply.github.com>
2024-02-29 14:22:56 -0800

    Implemented routing and Menu on Profile, Home, Favorites

--- better-instagram/src/main.jsx
+++ better-instagram/src/main.jsx
@@ -2,10 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AuthPage from './pages/AuthPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import FavoritesPage from './pages/FavoritesPage.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AuthPage/>,
+  },
+  {
+    path: '/home',
+    element: <HomePage/>,
+  },
+  {
+    path: '/profile',
+    element: <ProfilePage/>,
+  },
+  {
+    path: '/favorites',
+    element: <FavoritesPage/>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
         <App />
+        <RouterProvider router={router}/>
   </React.StrictMode>,
 );
 
--- better-instagram/src/pages/ProfilePage.jsx
+++ better-instagram/src/pages/ProfilePage.jsx
@@ -1,11 +1,13 @@
 import Cards from '../components/Profile/Cards'
 import AddPost from '../components/Profile/AddPost'
+import NavBar from '../components/NavBar/NavBar';
 
 
 
 function ProfilePage() {
     return (
         <>
+            <NavBar/>
             <AddPost />
             <Cards />
         </>
