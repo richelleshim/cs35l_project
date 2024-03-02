f28070d7f38b066757e797e1cd54d9f4fe256396
Chai112 <sirdesignpie@gmail.com>
2024-03-02 13:06:14 -0800

    separate createPosts button and modal

--- better-instagram/src/components/Profile/AddPost.jsx
+++ better-instagram/src/components/Profile/AddPost.jsx
@@ -1,8 +1,64 @@
 import { Box, Button, Modal, Textarea, IconButton, Typography } from '@mui/joy';
 import { useRef, useState } from 'react';
 
+export function PostWidget({ close }) {
+  return <Modal
+    open={true}
+    onClose={close}
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Box
+      sx={{
+        bgcolor: '#FFFFFF',
+        borderRadius: 10,
+        p: 1.5,
+        height: '75vh',
+        width: '60vh',
+        overflowY: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        level="h1"
+        sx={{ pt: 3 }}
+      >
+        Create Post
+      </Typography>
+      <Box
+        height={'50vh'}
+        width={'50vh'}
+        my={3}
+        sx={{
+          border: '1px dashed grey',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Button onClick={() => inputRef.current.click()}>Add image</Button>
+      </Box>
 
-export default function AddPost() {
+      <Textarea
+        placeholder="Add a caption"
+        minRows={4}
+        maxRows={4}
+        sx={{
+          borderRadius: '10px',
+          minWidth: '300px',
+        }}
+      />
+    </Box>
+  </Modal>;
+
+}
+
+export default function AddPostButton() {
   const [openModal, setOpenModal] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
   const inputRef = useRef(null);
@@ -14,17 +70,17 @@ export default function AddPost() {
     }
   };
 
-  const toggleModal = () => {
-    setOpenModal(!openModal);
-  };
-
   const handleClose = () =>{
     setSelectedFile(null);
   };
 
+  const click=()=>{
+      setOpenModal(!openModal);
+  }
+
   return (
     <>
-      <Button onClick={toggleModal}>Create post</Button>
+      <Button onClick={() => {click()}}>Create post</Button>
       <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
         <input
           type="file"
@@ -44,56 +100,9 @@ export default function AddPost() {
         </Box>
       )}
 
-      <Modal
-        open={openModal}
-        onClose={toggleModal}
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Box
-          sx={{
-            bgcolor: '#FFFFFF',
-            borderRadius: 10,
-            p: 1.5,
-            height: '75vh',
-            width: '60vh',
-            overflowY: 'auto',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography level="h1">
-            Create Post
-          </Typography>
-          <Box
-            height={'50vh'}
-            width={'50vh'}
-            my={3}
-            sx={{
-              border: '1px dashed grey',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Button onClick={() => inputRef.current.click()}>Add image</Button>
-          </Box>
-
-          <Textarea
-            placeholder="Add a caption"
-            minRows={4}
-            maxRows={4}
-            sx={{
-              borderRadius: '10px',
-              minWidth: '300px',
-            }}
-          />
-        </Box>
-      </Modal>
+      {openModal && <PostWidget
+        close={()=>{click()}}
+      />}
     </>
   );
 }
--- better-instagram/src/pages/ProfilePage.jsx
+++ better-instagram/src/pages/ProfilePage.jsx
@@ -1,12 +1,12 @@
 import Cards from '../components/Profile/Cards'
-import AddPost from '../components/Profile/AddPost'
+import AddPostButton from '../components/Profile/AddPost'
 
 
 
 function ProfilePage() {
     return (
         <>
-            <AddPost />
+            <AddPostButton />
             <Cards />
         </>
         
