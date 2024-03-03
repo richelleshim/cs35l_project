fe139c99e2becf09a6af0379113f8279798ba2b4
Chai112 <sirdesignpie@gmail.com>
2024-03-02 17:48:28 -0800

    fix bugs i accidentally pushed to main

--- better-instagram/src/pages/ProfilePage.jsx
+++ better-instagram/src/pages/ProfilePage.jsx
@@ -1,5 +1,7 @@
 import Cards from '../components/Profile/Cards'
 import AddPostButton from '../components/Profile/AddPost'
+import NavBar from '../components/NavBar/NavBar';
+import ProfileDetails from '../components/Profile/ProfileDetails'
 
 
 
