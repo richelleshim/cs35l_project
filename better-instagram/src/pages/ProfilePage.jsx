4553a6aaffa2a5d82a7d903b99d903c2ae0d6e82
y707171 <158012680+y707171@users.noreply.github.com>
2024-03-07 15:23:16 -0800

    i don't know what happened

--- better-instagram/src/pages/ProfilePage.jsx
+++ better-instagram/src/pages/ProfilePage.jsx
@@ -1,15 +1,29 @@
-import Cards from '../components/Profile/Cards'
-import AddPost from '../components/Profile/AddPost'
-
-
+import Cards from "../components/Profile/Cards";
+import AddPostButton from "../components/Profile/AddPost";
+import NavBar from "../components/NavBar/NavBar";
+import ProfileDetails from "../components/Profile/ProfileDetails";
+import { Box, Stack } from "@mui/joy";
 
 function ProfilePage() {
-    return (
-        <>
-            <AddPost />
-            <Cards />
-        </>
-        
-    );
+  const addedPost = () => {
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <Stack direction="row">
+        <NavBar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <ProfileDetails />
+          <AddPostButton
+            addedPost={() => {
+              addedPost();
+            }}
+          />
+          <Cards />
+        </Box>
+      </Stack>
+    </>
+  );
 }
 export default ProfilePage;
