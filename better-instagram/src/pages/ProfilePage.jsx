a75fc4338ebc7c46baa240538003e0c370ba692d
y707171 <158012680+y707171@users.noreply.github.com>
2024-03-06 19:40:18 -0800

    added header to ProfilePage

--- better-instagram/src/pages/ProfilePage.jsx
+++ better-instagram/src/pages/ProfilePage.jsx
@@ -1,11 +1,18 @@
 import Cards from '../components/Profile/Cards'
 import AddPost from '../components/Profile/AddPost'
 import ProfileDetails from '../components/Profile/ProfileDetails'
-
+import { Stack, Box } from "@mui/joy";
 
 function ProfilePage() {
     return (
         <>
+            <Box sx={{ pb: 5 }}>
+                <img
+                src="public/Bruingram.png"
+                loading="lazy"
+                alt=""
+                />
+            </Box>
             <ProfileDetails />
             <AddPost />
             <Cards />
