635927736b39cec80ad78a57723800e26d58f839
Chai112 <sirdesignpie@gmail.com>
2024-03-10 17:25:14 -0700

    update home page to new one

--- better-instagram/src/pages/HomePage.jsx
+++ better-instagram/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import HomePageWidget from "../components/Home/HomePageWidget";
 import FilterButton from "../components/Home/FilterButton";
 
 // import from MUI
-import { Stack } from "@mui/joy";
+import { Stack, Box } from "@mui/joy";
 import NavBar from "../components/NavBar/NavBar";
 import {
   getStorage,
@@ -13,7 +13,6 @@ import {
 import { getDocs, collection} from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 import { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom";
 
 function HomePage() {
   const [usersList, setUsersList] = useState([])
@@ -72,120 +71,28 @@ const handleGoToProfile =(uid)=>{
   navigate(`/profile?uid=${uid}`)
 }
 
-  return (
-    <>
-      <Stack direction="row">
-        <NavBar />
-              <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '10px' }}>
-                
-                {userWithImageList.map((user) => (
-                    
-                      <HomePageWidget
-                        key={user.id}
-                        name={user.fullName}
-                        desc={user.bio}
-                        major={user.major}
-                        year={user.year}
-                        imageSrc={user.profilePicURL}
-                        handleGoToProfile={() => handleGoToProfile(user.id)}
-                      />  
-                    
-                ))}
-              </div>
-      <Stack>
-        <FilterButton />
-      </Stack>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          pb: 5,
-          pt: 5, // Add a left margin to the content based on the navbar's width
-        }}
-      >
-        <Stack direction="column" alignItems="flex-start" spacing={3}>
-          <Stack
-            direction="row"
-            spacing={3}
-            alignItems="flex-start"
-            justifyContent="center"
-          >
-            <HomePageWidget
-              name="Daniel Fenex"
-              desc="Likes cats."
-              major="Computer Science"
-              year="25"
-              imageSrc="daniel"
-            />
-            <HomePageWidget
-              name="Jennifer Lee"
-              desc="I don't think YRL is good."
-              major="Cognitive Science"
-              year="26"
-              imageSrc="jennifer"
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={3}
-            alignItems="flex-start"
-            justifyContent="center"
-          >
-            <HomePageWidget
-              name="Richelle Shim"
-              desc="Hates dogs."
-              major="Chicano Studies"
-              year="26"
-              imageSrc="richelle"
-            />
-            <HomePageWidget
-              name="Michael J. Jordan"
-              desc="Takes things personally."
-              major="Chemical Sciences"
-              year="24"
-              imageSrc="mjj"
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={3}
-            alignItems="flex-start"
-            justifyContent="center"
-          >
-            <HomePageWidget
-              name="Michael B. Jordan"
-              desc='"Whats ur number bb"'
-              major="Colorado Studies"
-              year="26"
-              imageSrc="mjb"
-            />
-            <HomePageWidget
-              name="Chai Chai"
-              desc="I eat hamburgers."
-              major="Cantonese Studies"
-              year="26"
-              imageSrc="chai"
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={3}
-            alignItems="flex-start"
-            justifyContent="center"
-          >
-            <HomePageWidget
-              name="Elizabeth Moh"
-              desc="👍"
-              major="Calligraphy Sciences"
-              year="26"
-              imageSrc="elizabeth"
-            />
-          </Stack>
-        </Stack>
-      </Box>
-    </>
-  );
+  return <Stack direction="row">
+    <Stack>
+      <FilterButton />
+    </Stack>
+    <NavBar />
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '10px' }}>
+
+      {userWithImageList.map((user) => (
+
+        <HomePageWidget
+          key={user.id}
+          name={user.fullName}
+          desc={user.bio}
+          major={user.major}
+          year={user.year}
+          imageSrc={user.profilePicURL}
+          handleGoToProfile={() => handleGoToProfile(user.id)}
+        />
+
+      ))}
+    </div>
+  </Stack>;
 }
 
 export default HomePage;
