95065f0fb2299382f6146dd11eb4db10cd13bb65
Chai112 <sirdesignpie@gmail.com>
2024-03-11 22:57:44 -0700

    implement likes

--- better-instagram/src/pages/ProfilePage.jsx
+++ better-instagram/src/pages/ProfilePage.jsx
@@ -1,5 +1,4 @@
 import Cards from "../components/Profile/Cards";
-import AddPostButton from "../components/Profile/AddPost";
 import NavBar from "../components/NavBar/NavBar";
 import ProfileDetails from "../components/Profile/ProfileDetails";
 import useAuthStore from "../store/authStore";
