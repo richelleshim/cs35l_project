f91141154372e470f85c2e73da8a99f7c0df5d1e
danielfenex <68931226+danielfenex@users.noreply.github.com>
2024-03-11 21:25:02 -0700

    Update NavBar.jsx with Logout

--- better-instagram/src/components/NavBar/NavBar.jsx
+++ better-instagram/src/components/NavBar/NavBar.jsx
@@ -12,12 +12,12 @@ import {
   Paper,
   Stack,
 } from "@mui/material";
-
+import useLogout from "../../hooks/logout";
 
 export function NavBar() {
   // const [selected, setSelected] = useState("/home");
   const navigate = useNavigate();
-
+  const logout = useLogout();
   const handleChange = (newRoute) => {
     navigate(newRoute);
     setValue(newRoute);
@@ -75,6 +75,8 @@ export function NavBar() {
             onClick={() => handleChange("/profile")}
           />
         </BottomNavigation>
+
+        <Button onCLick={logout}>Logout</Button>
       </Stack>
     </Paper>
   );
