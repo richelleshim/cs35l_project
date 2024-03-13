1d01cd83d12c855877da5752f75a8eed4bf5b587
danielfenex <68931226+danielfenex@users.noreply.github.com>
2024-03-13 00:52:12 -0700

    Update firebase.js to not process deployment (for demo)

--- better-instagram/src/firebase/firebase.js
+++ better-instagram/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyByIX9VCoQqYqCUI1v_pjR1N4ARG_fAMGM",
+  authDomain: "instagram-clone-7638a.firebaseapp.com",
+  projectId: "instagram-clone-7638a",
+  storageBucket: "instagram-clone-7638a.appspot.com",
+  messagingSenderId: "896975114056",
+  appId: "1:896975114056:web:269837d4a5e06fe6d2ed7d",
+  measurementId: "G-8ZDRN72D4B"
 };
 
 const app = initializeApp(firebaseConfig);
